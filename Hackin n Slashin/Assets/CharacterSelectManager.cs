80b5711eb4bc187fa281b9b4aea3ad8cd8d46a72
Matthew Cresswell <54798864+MatthewCresswell@users.noreply.github.com>
2024-10-03 00:22:19 +0200

    Character Select Scene updated

--- Hackin n Slashin/Assets/CharacterSelectManager.cs
+++ Hackin n Slashin/Assets/CharacterSelectManager.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.TextCore.Text;
 
 public class CharacterSelectManager : MonoBehaviour
 {
+    public GetCharacter getCharacterScript;
     public GameObject Char1_IMG;
     public GameObject Char2_IMG;
     public GameObject Char3_IMG;
@@ -16,8 +20,11 @@ public class CharacterSelectManager : MonoBehaviour
     public GameObject P2CharSelectedIMG;
     public string Player1_Char;
     public string Player2_Char;
+    public bool Player1Turn;
+    public bool Player1HasSelected;
+    public bool Player2Turn;
+    public bool Player2HasSelected;
 
-    private List<PlayerConfiguration> playerConfigs;
 
     [SerializeField] private int MaxPlayers = 2;
     public static CharacterSelectManager Instance { get; private set; }
@@ -31,44 +38,66 @@ public class CharacterSelectManager : MonoBehaviour
         else
         {
             Instance = this;
-            DontDestroyOnLoad(Instance);
-            playerConfigs = new List<PlayerConfiguration>();
         }
     }
 
-    public void SetPlayerChar(int index, string CharName)
-    {
-        playerConfigs[index].PlayerName = CharName;
-    }
-
-    public void ReadyPlayer(int index)
-    {
-        playerConfigs[index].IsReady = true;
-    }
-
-    // Start is called before the first frame update
     void Start()
     {
-        
+        Player1Turn = true;
+        Player2Turn = false;
+        Player1HasSelected = false;
+        Player2HasSelected = false;
+    }
+
+    public void SwitchP1toP2()
+    {
+        if (Player1Turn == false)
+        {
+            Player2Turn = true;
+        }
+    }
+
+    public void CharactersSelectedCheck()
+    {
+        if (Player1HasSelected && Player2HasSelected)
+        {
+            StartCoroutine(ChangeScene());
+        }
+    }
+
+    public void CharacterHasBeenSelected()
+    {
+        if (Player1Turn == true)
+        {
+            Debug.Log("This script is working");
+            Char1_IMG.SetActive(true);
+            Player1Turn = false;
+            Player1HasSelected = true;
+            Player2Turn = true;
+            //getCharacterScript.HasBeenSelected();
+        }
+        if (Player2Turn == true)
+        {
+            Player2Turn = false;
+            Player2HasSelected = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        CharactersSelectedCheck();
     }
-}
 
-public class PlayerConfiguration
-{ 
-    public PlayerConfiguration(PlayerInput pi)
+    IEnumerator ChangeScene()
     {
-        PlayerIndex = pi.playerIndex;
-        Input = pi;
+        while (true) 
+        {
+            yield return new WaitForSeconds(20f);
+            SceneManager.LoadScene("Matts_Test_Scene");            
+        }
     }
-    public PlayerInput Input { get; set; }
-    public int PlayerIndex { get; set; }
-    public string PlayerName { get; set; }
-    public bool IsReady { get; set; }
 }
+
+
 
--- Hackin n Slashin/Assets/GetCharacter.cs
+++ Hackin n Slashin/Assets/GetCharacter.cs
@@ -1,32 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 
 public class GetCharacter : MonoBehaviour
 {
     public CharacterSelectManager charSelMan;
+    public GameObject character;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //character.SetActive(false);
     }
 
-    public void OnMouseDown()
+    public void HasBeenSelected()
     {
-        if (this.CompareTag("Player 1"))
+        if (charSelMan.Player1Turn == true)
         {
             charSelMan.Player1_Char = this.gameObject.tag;
+            Debug.Log("tag is " + charSelMan.Player1_Char);
         }
-        else if (this.CompareTag("player 2"))
+        if (charSelMan.Player2Turn == true)
         {
             charSelMan.Player2_Char = this.gameObject.tag;
+            Debug.Log("tag is " + charSelMan.Player2_Char);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        OnMouseDown();
+        
     }
 }
