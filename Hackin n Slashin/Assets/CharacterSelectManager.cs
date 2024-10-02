8c6807a0ff8cff9b6bec6ce2ce4f91d3d7d040f7
Matthew Cresswell <54798864+MatthewCresswell@users.noreply.github.com>
2024-10-02 20:28:08 +0200

    Character Select Menu updates

---
+++ Hackin n Slashin/Assets/CharacterSelectManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelectManager : MonoBehaviour
+{
+    public GameObject Char1_IMG;
+    public GameObject Char2_IMG;
+    public GameObject Char3_IMG;
+    public GameObject Char4_IMG;
+    public GameObject Char5_IMG;
+    public GameObject Char6_IMG;
+    public GameObject Char7_IMG;
+    public GameObject P1CharSelectedIMG;
+    public GameObject P2CharSelectedIMG;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
